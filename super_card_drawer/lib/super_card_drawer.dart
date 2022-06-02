97182687637e5786b12e8e3af9ecb6c4e8165d36
Parker Hutchinson <parkerhutchinson@Parkers-MacBook-Pro.local>
2022-06-02 09:46:53 -0700

    initialized the package template in my lib package.

--- super_card_drawer/lib/super_card_drawer.dart
+++ super_card_drawer/lib/super_card_drawer.dart
@@ -1,4 +1,6 @@
 library super_card_drawer;
 
+// ignore: depend_on_referenced_packages
 export 'package:card_reveal_drawer/card_reveal_drawer.dart';
+// ignore: depend_on_referenced_packages
 export 'package:card_expand_drawer/card_expand_drawer.dart' hide Direction;
